f96f17a7908aba4c23eee00f4d7854c8135a9afd
nurazon59 <nurazon59@users.noreply.github.com>
2026-05-05 00:27:40 +0900

    chore

--- cmd/run.go
+++ cmd/run.go
@@ -3,7 +3,7 @@ package cmd
 import (
 	"github.com/adrg/xdg"
 	"github.com/alecthomas/kong"
-	template "github.com/nurazon59/pinpm"
+	pinpm "github.com/nurazon59/pinpm"
 )
 
 const appVersion = "v0.1.0"
